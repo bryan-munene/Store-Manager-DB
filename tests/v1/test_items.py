eef058eacf9e8b55fdd13c2ec1c150d06de51f39
bryan-munene <bryanmuthuri38@gmail.com>
2018-11-02 01:20:54 +0300

    [Chore #161642280] add the assert check for 'status' in items tests

--- tests/v1/test_items.py
+++ tests/v1/test_items.py
@@ -44,14 +44,14 @@ class Test_Items(Store_Manager_Base):
     def test_items_retrive_all_no_item(self):
         response= self.test_client.get('/api/v2/items',content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
+        assert(msg['status'] == "not found")
         assert(response.status_code==404)
 
 
     def test_items_retrive_all_successfully(self):
         response= self.test_client.get('/api/v2/items',content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
+        assert(msg['status'] == "ok")
         assert(response.status_code==200)
 
     '''-------------------------------------------------------------------------------------------------------------------------------'''
@@ -62,55 +62,55 @@ class Test_Items(Store_Manager_Base):
     def test_items_price_not_digit(self):
         response= self.test_client.post('/api/v2/add_item', data=sample_item[0] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==400)
+        assert(msg['status'] == "not acceptable")
+        assert(response.status_code==406)
 
     def test_items_price_not_digit1(self):
         response= self.test_client.post('/api/v2/add_item', data=sample_item[1] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==400)
+        assert(msg['status'] == "not acceptable")
+        assert(response.status_code==406)
 
     def test_items_item_name_not_str(self):
         response= self.test_client.post('/api/v2/add_item', data=sample_item[2] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==400)
+        assert(msg['status'] == "not acceptable")
+        assert(response.status_code==406)
 
     def test_items_item_name_empty(self):
         response= self.test_client.post('/api/v2/add_item', data=sample_item[3] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==400)
+        assert(msg['status'] == "not acceptable")
+        assert(response.status_code==406)
 
     def test_items_price_empty(self):
         response= self.test_client.post('/api/v2/add_item', data=sample_item[4] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==400)
+        assert(msg['status'] == "not acceptable")
+        assert(response.status_code==406)
 
     def test_items_image_empty(self):
         response= self.test_client.post('/api/v2/add_item', data=sample_item[5] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==400)
+        assert(msg['status'] == "not acceptable")
+        assert(response.status_code==406)
 
     def test_items_quantity_not_digit(self):
         response= self.test_client.post('/api/v2/add_item', data=sample_item[6] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==400)
+        assert(msg['status'] == "not acceptable")
+        assert(response.status_code==406)
 
     def test_items_quantity_not_digit1(self):
         response= self.test_client.post('/api/v2/add_item', data=sample_item[7] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==400)
+        assert(msg['status'] == "not acceptable")
+        assert(response.status_code==406)
 
     def test_items_successfully(self):
         response= self.test_client.post('/api/v2/add_item', data=json.dumps(sample_item[8]) ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
+        assert(msg['status'] == "created")
         assert(response.status_code==201)
 
     '''-------------------------------------------------------------------------------------------------------------------------------'''
@@ -122,19 +122,19 @@ class Test_Items(Store_Manager_Base):
     def test_get_item_negative_identifier(self):
         response= self.test_client.get('/api/v2/items/-1' ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
+        assert(msg['status'] == "not found")
         assert(response.status_code == 404)
 
     def test_get_item_not_created(self):
         response= self.test_client.get('/api/v2/items/100' ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
+        assert(msg['status'] == "not found")
         assert(response.status_code == 404)
 
     def test_get_item_successfully(self):
         response= self.test_client.get('/api/v2/items/1' ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
+        assert(msg['status'] == "ok")
         assert(response.status_code == 200)
 
 
@@ -143,13 +143,13 @@ class Test_Items(Store_Manager_Base):
     def test_get_items_by_category_not_created(self):
         response= self.test_client.get('/api/v2/items/painkillers' ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
+        assert(msg['status'] == "not found")
         assert(response.status_code == 404)
 
     def test_get_items_successfully(self):
         response= self.test_client.get('/api/v2/items/antibiotics' ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
+        assert(msg['status'] == "ok")
         assert(response.status_code == 200)
 
     '''-------------------------------------------------------------------------------------------------------------------------------'''
@@ -161,80 +161,80 @@ class Test_Items(Store_Manager_Base):
     def test_update_item_nonexistent(self):
         response= self.test_client.put('/api/v2/items/100', data=sample_item_updates[0] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==400)
+        assert(msg['status'] == "not found")
+        assert(response.status_code==404)
 
     def test_items_update_price_not_digit(self):
         response= self.test_client.put('/api/v2/items/1', data=sample_item_updates[1] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==405)
+        assert(msg['status'] == "not acceptable")
+        assert(response.status_code==406)
 
     def test_items_update_price_not_digit1(self):
         response= self.test_client.put('/api/v2/items/1', data=sample_item_updates[2] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==400)
+        assert(msg['status'] == "not acceptable")
+        assert(response.status_code==406)
         
     def test_items_update_item_name_not_str(self):
         response= self.test_client.put('/api/v2/add_item', data=sample_item_updates[3] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==405)
+        assert(msg['status'] == "not acceptable")
+        assert(response.status_code==406)
 
     def test_items_update_item_name_empty(self):
         response= self.test_client.put('/api/v2/items/1', data=sample_item_updates[4] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==400)
+        assert(msg['status'] == "not acceptable")
+        assert(response.status_code==406)
 
     def test_items_update_quantity_not_digit(self):
         response= self.test_client.put('/api/v2/items/1', data=sample_item_updates[5] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==405)
+        assert(msg['status'] == "not acceptable")
+        assert(response.status_code==406)
 
     def test_items_update_quantity_not_digit1(self):
         response= self.test_client.put('/api/v2/items/1', data=sample_item_updates[6] ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==400)
+        assert(msg['status'] == "not acceptable")
+        assert(response.status_code==406)
 
     def test_update_item_empty(self):
         response= self.test_client.put('/api/v2/items/1', data=json.dumps(sample_item_updates[7]) ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
+        assert(msg['status'] == "forbidden")
         assert(response.status_code==403)
 
     def test_update_item_price_only_successfully(self):
         response= self.test_client.put('/api/v2/items/1', data=json.dumps(sample_item_updates[8]) ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==200)
+        assert(msg['status'] == "created")
+        assert(response.status_code==201)
 
     def test_update_item_name_only_successfully(self):
         response= self.test_client.put('/api/v2/items/1', data=json.dumps(sample_item_updates[9]) ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==200)
+        assert(msg['status'] == "created")
+        assert(response.status_code==201)
 
     def test_update_item_quantity_only_successfully(self):
         response= self.test_client.put('/api/v2/items/1', data=json.dumps(sample_item_updates[10]) ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==200)
+        assert(msg['status'] == "created")
+        assert(response.status_code==201)
 
     def test_update_item_image_only_successfully(self):
         response= self.test_client.put('/api/v2/items/1', data=json.dumps(sample_item_updates[11]) ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==200)
+        assert(msg['status'] == "created")
+        assert(response.status_code==201)
 
     def test_update_item_all_successfully(self):
         response= self.test_client.put('/api/v2/items/1', data=json.dumps(sample_item_updates[12]) ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
-        assert(response.status_code==200)
+        assert(msg['status'] == "created")
+        assert(response.status_code==201)
 
     '''-------------------------------------------------------------------------------------------------------------------------------'''
 
@@ -244,19 +244,19 @@ class Test_Items(Store_Manager_Base):
     def test_delete_item_negative_identifier(self):
         response= self.test_client.delete('/api/v2/items/-1' ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
+        assert(msg['status'] == "not found")
         assert(response.status_code == 404)
 
     def test_delete_item_not_created(self):
         response= self.test_client.delete('/api/v2/items/100' ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
+        assert(msg['status'] == "not found")
         assert(response.status_code == 404)
 
     def test_delete_item_successfully(self):
         response= self.test_client.delete('/api/v2/items/1' ,content_type='application/json')
         msg = json.loads(response.data.decode('utf-8'))
-        assert(msg['status'] == "")
+        assert(msg['status'] == "ok")
         assert(response.status_code == 200)
 
 
